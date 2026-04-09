58195a021c5156b8ffb3ce20ae95309fc701660d
karnikavt-ops <karnikavt@gmail.com>
2026-04-09 10:16:29 +0530

    Time: 21 ms (79.58%), Space: 59.7 MB (26.67%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,15 +1,15 @@
-import java.util.*;
 class RecentCounter {
-    Queue<Integer> q;
+    Queue<Integer>q;
     public RecentCounter() {
         q = new LinkedList<>();
-    }   
+    }
+    
     public int ping(int t) {
-        q.add(t);  
-        while (q.peek() < t - 3000) {
+        q.add(t);
+        while(q.peek() < (t-3000)){
             q.poll();
         }
-        return q.size();
+    return q.size();
     }
 }
 
