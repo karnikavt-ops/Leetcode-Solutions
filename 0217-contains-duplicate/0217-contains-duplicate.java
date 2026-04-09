2f8eae25d83e8d2d801633f464aaa28f768711d1
karnikavt-ops <karnikavt@gmail.com>
2026-04-09 12:24:55 +0530

    Time: 14 ms (89.09%), Space: 93.6 MB (43.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;   // duplicate found
+            }
+            set.add(nums[i]);  // store number
+        }
+        return false;  // no duplicates
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
