676af79e5b6ffcee5ccac30ca68f65ac1eff6ff2
karnikavt-ops <karnikavt@gmail.com>
2026-04-09 12:52:29 +0530

    Time: 1 ms (45.17%), Space: 43.5 MB (45.56%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if(head==null) return head;
+
+        Node curr = head;
+        while(curr!=null){
+            if(curr.child!=null){
+                Node up = curr.next;
+                curr.next = flatten(curr.child);
+                curr.next.prev=curr;
+                curr.child=null;
+                while(curr.next!=null){
+                    curr=curr.next;
+                }
+                if(up!=null){
+                    curr.next=up;
+                    up.prev=curr;
+                }
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
