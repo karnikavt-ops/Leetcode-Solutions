a767c2956ef3e9b64e369b0b9e97820de4c0dc97
karnikavt-ops <karnikavt@gmail.com>
2026-04-13 14:47:33 +0530

    Time: 4 ms (3.03%), Space: 46.6 MB (14.02%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> stack = new Stack<>();
-        int j=0;
-        for(int n: pushed){
-            stack.push(n);
-            while(!stack.isEmpty()&& (stack.peek()==popped[j])){
-                stack.pop();
-                j++;
-            }                
+        Stack<Integer> st = new Stack<>();
+        Queue<Integer> ip = new LinkedList<>();
+        Queue<Integer> op = new LinkedList<>();
+
+        for (int val : pushed) {
+            ip.add(val);
         }
-        return stack.isEmpty();
+        for (int val : popped) {
+            op.add(val);
+        }
+
+        while (!ip.isEmpty()) {
+            st.push(ip.poll());
+
+            while (!st.isEmpty() && !op.isEmpty() && st.peek().equals(op.peek())) {
+                st.pop();
+                op.poll();
+            }
+        }
+
+        return st.isEmpty();
     }
 }
