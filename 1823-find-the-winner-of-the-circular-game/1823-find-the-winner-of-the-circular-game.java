3cf0ee61468ddc1785acf8ba3ad148814d56a3db
karnikavt-ops <karnikavt@gmail.com>
2026-04-29 09:43:41 +0530

    Time: 2 ms (61.77%), Space: 42.6 MB (41.82%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,12 +1,17 @@
 class Solution {
-    public int find(int n, int k){
-        if(n==1){
-            return 0;
-        }
-        return(find(n-1,k)+k) %n;
-    }
     public int findTheWinner(int n, int k) {
-            int result = find(n,k);
-            return result+1;
+        List<Integer> people = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            people.add(i);
+        }
+        return kill(people,k,0);
+    }
+
+    public int kill(List<Integer> people, int k , int index){
+        if(people.size()==1)
+            return people.get(0);
+        index = (index+k-1)%people.size();
+        people.remove(index);
+        return kill(people,k,index);
     }
 }
