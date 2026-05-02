4ae63535c48ba4b7aa04444f2ab907f628ec4120
karnikavt-ops <karnikavt@gmail.com>
2026-05-02 11:10:44 +0530

    Time: 2 ms (72.37%), Space: 44.9 MB (34.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,17 +1,19 @@
 class Solution {
-    void backTrack(List<String>res,String s,int op,int cl,int n){
-        if(op<n)
-            backTrack(res,s+"(",op+1,cl,n);
-        if(cl<op)
-            backTrack(res,s+")",op,cl+1,n);
+    void backTrack(List<String>res, String s, int opn, int clo, int n){
         if(s.length()==n*2){
             res.add(s);
             return;
         }
+        if(opn<n)
+            backTrack(res, s+"(", opn+1, clo, n);
+        if(clo<opn)
+            backTrack(res, s+")", opn, clo+1, n);
+        
     }
+
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backTrack(res,"",0,0,n);
+        backTrack(res, "", 0 , 0, n);
         return res;
     }
 }
