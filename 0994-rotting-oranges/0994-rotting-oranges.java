86f5a6938711a203fac4baf7099e3fe79e92d29e
karnikavt-ops <karnikavt@gmail.com>
2026-06-22 11:02:11 +0530

    Time: 1 ms (100%), Space: 44 MB (83.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,47 +1,42 @@
 class Solution {
-    public void rot(int[][] grid, int[][] timeGrid, int i, int j, int day) {
-        if(i < 0 || j < 0 ||
-           i >= grid.length || j >= grid[0].length ||
-           grid[i][j] == 0 ||
-           timeGrid[i][j] <= day)
+    public void rot(int [][] grid , int [][] timegrid , int i , int j , int day){
+        if(i>=grid.length || j>=grid[0].length || i<0 || j<0 ||grid[i][j]==0 || timegrid[i][j]<=day)
             return;
-
-        timeGrid[i][j] = day;
-        rot(grid, timeGrid, i + 1, j, day + 1);
-        rot(grid, timeGrid, i - 1, j, day + 1);
-        rot(grid, timeGrid, i, j + 1, day + 1);
-        rot(grid, timeGrid, i, j - 1, day + 1);
+        timegrid[i][j] = day; 
+        rot(grid , timegrid, i+1 , j , day+1);
+        rot(grid , timegrid, i-1 , j , day+1);
+        rot(grid , timegrid, i , j+1 , day+1);
+        rot(grid , timegrid, i , j-1 , day+1);
     }
 
     public int orangesRotting(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
+        int row = grid.length ;
+        int col = grid[0].length ;
 
-        int[][] timeGrid = new int[row][col];
-        for(int i = 0; i < row; i++) {
-            Arrays.fill(timeGrid[i], Integer.MAX_VALUE);
+        int [][] timegrid = new int [row][col] ;
+        for(int i=0; i<row ;i++){
+            Arrays.fill(timegrid[i], Integer.MAX_VALUE) ;
         }
-
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(grid[i][j] == 2) {
-                    rot(grid, timeGrid, i, j, 0);
+        for(int i=0; i<row ;i++){
+            for(int j=0 ; j<col ; j++){
+                if(grid[i][j] ==2){
+                    rot(grid, timegrid, i ,j ,0);
                 }
             }
         }
 
-        int dayCount = 0;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(grid[i][j] == 1) {
-                    if(timeGrid[i][j] == Integer.MAX_VALUE)
+        int min = 0;
+        for(int i=0; i<row ;i++){
+            for(int j=0 ; j<col ; j++){
+                if(grid[i][j]==1){
+                    if(timegrid[i][j]==Integer.MAX_VALUE)
                         return -1;
-
-                    dayCount = Math.max(dayCount, timeGrid[i][j]);
+                    min = Math.max(min, timegrid[i][j]);
                 }
             }
         }
 
-        return dayCount;
+        return min;
+
     }
 }
