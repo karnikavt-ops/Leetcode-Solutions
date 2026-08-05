eca89ef8da5ec3a7abeec7d07ab0ba00710483aa
karnikavt-ops <karnikavt@gmail.com>
2026-08-05 19:15:33 +0530

    Time: 0 ms (100%), Space: 15 MB (68.25%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -0,0 +1,9 @@
+int* getConcatenation(int* nums, int numsSize, int* returnSize) {
+    int *ans = (int *)malloc(sizeof(int) * 2 * numsSize);
+    for(int i = 0; i < numsSize; i++){
+        ans[i] = nums[i];
+        ans[i + numsSize] = nums[i];
+    }
+    *returnSize = 2 * numsSize;
+    return ans;
+}
