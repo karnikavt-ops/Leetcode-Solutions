20d3417cb938aeda97efccd15bc95105a793fc16
karnikavt-ops <karnikavt@gmail.com>
2026-08-05 11:38:19 +0530

    Time: 0 ms (100%), Space: 10.5 MB (29.48%) - LeetHub

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -1,7 +1,7 @@
 int removeElement(int* nums, int numsSize, int val) {
     int k = 0;
-    for (int i = 0; i < numsSize; i++) {
-        if (nums[i] != val) {
+    for(int i =0 ; i<numsSize; i++){
+        if(nums[i]!=val){
             nums[k] = nums[i];
             k++;
         }
