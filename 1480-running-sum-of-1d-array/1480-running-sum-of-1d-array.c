55ac1dcbf8fda058499d80a8e1644aad48f8fee3
karnikavt-ops <karnikavt@gmail.com>
2026-08-06 12:25:56 +0530

    Time: 0 ms (100%), Space: 12 MB (52.77%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -1,6 +1,6 @@
 int* runningSum(int* nums, int numsSize, int* returnSize) {
     int * result;
-    result =malloc(numsSize * sizeof(int));
+    result =malloc(numsSize * sizeof(nums));
     result[0] = nums[0];
     for(int i = 1; i < numsSize; i++){
         result[i] = result[i-1] + nums[i];
