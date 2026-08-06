f3f431ec5765376a11be8c25d3ff03db6b854954
karnikavt-ops <karnikavt@gmail.com>
2026-08-06 15:23:43 +0530

    Time: 0 ms (100%), Space: 15.3 MB (55.65%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.c
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.c
@@ -1,8 +1,9 @@
 int* buildArray(int* nums, int numsSize, int* returnSize) {
-    int *ans = (int *)malloc(numsSize * sizeof(int));
-    for(int i = 0; i < numsSize; i++){
-        ans[i] = nums[nums[i]];
-    }
-    *returnSize = numsSize;
-    return ans;
+    int * res ;
+    res = malloc(sizeof(int)*numsSize);
+    for(int i=0 ;i<numsSize; i++){
+        res[i] = nums[nums[i]];
+    }    
+    *returnSize =  numsSize;
+    return res;
 }
