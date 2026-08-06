67914aa41860c0387642665b527744699807b0f0
karnikavt-ops <karnikavt@gmail.com>
2026-08-06 19:22:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (16.88%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,16 @@
+int lengthOfLastWord(char* s) {
+    int i = 0;
+    while (s[i] != '\0') {
+        i++;
+    }
+    i--; 
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+    int count = 0;
+    while (i >= 0 && s[i] != ' ') {
+        count++;
+        i--;
+    }
+    return count;
+}
