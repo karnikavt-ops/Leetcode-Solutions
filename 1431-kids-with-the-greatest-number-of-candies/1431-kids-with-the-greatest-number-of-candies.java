84642b6cdc551773c40d65eed979c4630346e92c
karnikavt-ops <karnikavt@gmail.com>
2026-08-20 19:37:28 +0530

    Time: 1 ms (96.33%), Space: 43.6 MB (90.75%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        // Find the maximum candies
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+        // Check each child
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= max);
+        }
+        return result;
+    }
+}
